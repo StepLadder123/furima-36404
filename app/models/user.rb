ff9fa0a8a066359f928236d33f883c4df284faf5
StepLadder123 <makito.takatori@gmail.com>
2021-08-10 20:25:50 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :birth_date
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
     validates :last_name
     validates :first_name
   end
@@ -13,5 +16,4 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :first_name_kana
   end
-  validates :birth_date, presence: true
 end
