55a9d2872aeb3020ec39e6d8dab5b142addf7de9
StepLadder123 <makito.takatori@gmail.com>
2021-08-11 14:06:37 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_choice
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :price
+    validates :user, foreign_key: true
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :ship_fee_id
+    validates :ship_area_id
+    validates :ship_schedule_id
+  end
 end
