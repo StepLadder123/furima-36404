6ffc86d9b29fd0d22f9c795704d4d53ac055b065
StepLadder123 <makito.takatori@gmail.com>
2021-08-11 22:27:11 +0900

    FactoryBot生成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name             {Faker::Name.name}
+    info             {Faker::Lorem.sentence}
+    category_id      {Faker::Number.between(from: 2, to: 10)}
+    status_id        {Faker::Number.between(from: 2, to: 7)}
+    ship_fee_id      {Faker::Number.between(from: 2, to: 3)}
+    ship_area_id     {Faker::Number.between(from: 2, to: 48)}
+    ship_schedule_id {Faker::Number.between(from: 2, to: 4)}
+    price            {Faker::Number.between(from: 300, to: 9999999)}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
