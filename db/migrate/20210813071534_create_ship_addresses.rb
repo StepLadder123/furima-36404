95a005e14dafffe301229b036d225faaefb8a61b
StepLadder123 <makito.takatori@gmail.com>
2021-08-13 16:23:39 +0900

    マイグレーションの記述

--- db/migrate/20210813071534_create_ship_addresses.rb
+++ db/migrate/20210813071534_create_ship_addresses.rb
@@ -1,8 +1,22 @@
 class CreateShipAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :ship_addresses do |t|
-
+      t.string     :postal_code,   null: false
+      t.integer    :prefecture_id, null: false
+      t.string     :city,          null: false
+      t.string     :address,       null: false
+      t.string     :build_name
+      t.string     :phone_number,  null: false
+      t.references :purchases,     null: false, foreign_key: true
       t.timestamps
     end
   end
 end
+
+| postal_code   | string     | null: false                    |
+| prefecture_id | integer    | null: false                    |
+| city          | string     | null: false                    |
+| address       | string     | null: false                    |
+| build_name    | string     |                                |
+| phone_number  | string     | null: false                    |
+| purchase      | references | null: false, foreign_key: true |
