c6d140ab1f6ee973c2b9db2122d4c7732d5781de
StepLadder123 <makito.takatori@gmail.com>
2021-08-16 15:11:58 +0900

    FactoryBotの作成

--- spec/factories/purchase_ship_addresses.rb
+++ spec/factories/purchase_ship_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_ship_address do
-    
+    postal_code { '123-4567'}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    city {'新宿区'}
+    address {'新宿1-1'}
+    build_name {'新宿ビル12F'}
+    phone_number {'0123456789'}
   end
 end
