0ef4d16fd0e03c6eeae9ff0d2a124a4173ad645e
StepLadder123 <makito.takatori@gmail.com>
2021-08-16 18:07:34 +0900

    バリデーションの追加

--- app/models/purchase_ship_address.rb
+++ app/models/purchase_ship_address.rb
@@ -1,6 +1,6 @@
 class PurchaseShipAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :build_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :build_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Input 8 half-width characters (include hyphen(-))."}
@@ -8,6 +8,7 @@ class PurchaseShipAddress
     validates :city
     validates :address
     validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/, message: "is invalid. Input 10 or 11 half-width characters (not include hyphen(-))."}
+    validates :token
   end
 
   def save
