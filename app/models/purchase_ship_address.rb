71e59c2db9468423b9189bad0ef5c95de6182664
StepLadder123 <makito.takatori@gmail.com>
2021-08-16 15:11:47 +0900

    バリデーションの作成

---
+++ app/models/purchase_ship_address.rb
@@ -0,0 +1,17 @@
+class PurchaseShipAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :build_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Input 8 characters (include hyphen(-))."}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/, message: "is invalid. Input 10 or 11 characters (not include hyphen(-))."}
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShipAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, build_name: build_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
