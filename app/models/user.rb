0d75cac9395af282ae5917028f2b261d0ac7f710
StepLadder123 <makito.takatori@gmail.com>
2021-08-18 22:40:20 +0900

    userモデルの修正
    ・passwordカラムのバリデーションをcreateアクション時のみに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }, on: :create
     validates :birth_date
   end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
