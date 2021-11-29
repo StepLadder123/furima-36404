34b6a80175df1c4406d53d93d32f6cd4b870544c
StepLadder123 <makito.takatori@gmail.com>
2021-11-29 15:03:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"
+  get 'furima-36404.herokuapp.com', to: redirect('www.yahoo.co.jp', status: 301)
   resources :items do
     resources :purchases, only: [:index, :create]
     collection do
